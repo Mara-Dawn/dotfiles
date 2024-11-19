37d97b6b87e2493c3d2b57b6b388e0e4fb0d397b
Mara <mara.dawn69@gmail.com>
2024-11-19 18:34:38 +0100

    dotfile updates

--- scripts/wallpaper_toggle.sh
+++ scripts/wallpaper_toggle.sh
@@ -2,11 +2,11 @@ if pgrep -x "linux-wallpaper" > /dev/null
 then
     echo "Running"
     pkill "linux-wallpaper"
-    hyprctl hyprpaper wallpaper "DP-3,~/.config/background.png"
+    hyprctl hyprpaper wallpaper "DP-3,~/.config/background_flipped.png"
     notify-send --expire-time 1350 "wallpaper-engine OFF"
 else
     echo "Stopped"
-    linux-wallpaperengine --no-audio-processing --silent --screen-root DP-3 --fps 30 2249576063 &> /dev/null & disown
+    linux-wallpaperengine --no-audio-processing --silent --screen-root DP-3 --fps 30 1682963130 &> /dev/null & disown
     notify-send --expire-time 1350 "wallpaper-engine ON" &
 fi
 

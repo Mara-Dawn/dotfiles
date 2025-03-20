2a55ccb3eed0af3b884d77921911caf035a5f219
Mara <mara.dawn69@gmail.com>
2025-03-20 20:05:20 +0100

    updates

--- scripts/wallpaper_toggle.sh
+++ scripts/wallpaper_toggle.sh
@@ -6,7 +6,7 @@ then
     notify-send --expire-time 1350 "wallpaper-engine OFF"
 else
     echo "Stopped"
-    linux-wallpaperengine --no-audio-processing --silent --screen-root DP-3 --fps 30 1682963130 &> /dev/null & disown
+    linux-wallpaperengine --no-audio-processing --silent --screen-root DP-3 --fps 30 3251872199 &> /dev/null & disown
     notify-send --expire-time 1350 "wallpaper-engine ON" &
 fi
 

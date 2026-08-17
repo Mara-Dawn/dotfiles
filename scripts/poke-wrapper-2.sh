cf1aa9058bd790c7770e5c19224d527409d940d6
Mara <github@mara.cafe>
2026-08-17 10:47:55 +0200

    updates

---
+++ scripts/poke-wrapper-2.sh
@@ -0,0 +1,8 @@
+WINEPREFIX="/home/mara/.local/share/Steam/steamapps/compatdata/3754784465/pfx"
+APP_PATH="/home/mara/.local/share/Steam/steamapps/compatdata/3754784465/pfx/drive_c/users/steamuser/The Pokémon Company International/Pokémon Trading Card Game Live/Pokemon TCG Live.exe"
+
+export STEAM_COMPAT_DATA_PATH=/home/mara/.local/share/Steam/steamapps/compatdata/3754784465
+export STEAM_COMPAT_CLIENT_INSTALL_PATH=/home/mara/.local/share/Steam/steamapps/compatdata/3754784465
+export WINE_CPU_TOPOLOGY=2
+
+proton-ge run "/home/mara/.local/share/Steam/steamapps/compatdata/3754784465/pfx/drive_c/users/steamuser/The Pokémon Company International/Pokémon Trading Card Game Live/Pokemon TCG Live.exe" -start "tpcitcgapp://callback?code=ory_ac_YPLox8E8e04E93jvEQl04N-RwPlGjTE3bX01INgi8eY.LFbrn5K424vVP7Ko1bSAPRrFKI093Oo3B3ERl2vyxGE&scope=offline+screen_name+openid+friends&state=qAfQRzcc0T3V1eoMPcPs85XcqUnxDluxTXNWjrV6eS0"
---
+++ scripts/poke-wrapper.sh
@@ -0,0 +1,8 @@
+WINEPREFIX="/home/mara/.local/share/Steam/steamapps/compatdata/3754784465/pfx"
+APP_PATH="/home/mara/.local/share/Steam/steamapps/compatdata/3754784465/pfx/drive_c/users/steamuser/The Pokémon Company International/Pokémon Trading Card Game Live/Pokemon TCG Live.exe"
+
+export STEAM_COMPAT_DATA_PATH=/home/mara/.local/share/Steam/steamapps/compatdata/3754784465
+export STEAM_COMPAT_CLIENT_INSTALL_PATH=/home/mara/.local/share/Steam/steamapps/compatdata/3754784465
+export WINE_CPU_TOPOLOGY=2
+
+proton-ge run "/home/mara/.local/share/Steam/steamapps/compatdata/3754784465/pfx/drive_c/users/steamuser/The Pokémon Company International/Pokémon Trading Card Game Live/Pokemon TCG Live.exe"
